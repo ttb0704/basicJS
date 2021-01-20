e8e1453e6a5adda4458065c480dad81ed6087db7
ttb0704 <ttb0704@naver.com>
2021-01-21 06:51:57 +0900

    Initial Commit

---
+++ clock.js
@@ -0,0 +1,24 @@
+const clockContainer = document.querySelector(".js-clock");
+const clock = clockContainer.querySelector("span");
+
+function loadClock(){
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    let time;
+
+    // clock.innerText = `${hours} : ${minutes} : ${seconds}`;
+    // `${hours<10?`0${hours}`:hours} : ${minutes<10?`0${minutes}`:minutes} : ${seconds<10?`0${seconds}`:seconds}`;
+    time = `${hours<10?`0${hours}`:hours} : `;
+    time += `${minutes<10?`0${minutes}`:minutes} : `;
+    time += `${seconds<10?`0${seconds}`:seconds}`;
+    console.log(time);
+    clock.innerText = time;
+    }
+
+
+function init(){
+    setInterval(loadClock,1000);
+}
+init();
