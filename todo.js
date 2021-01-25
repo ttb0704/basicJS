ad3c1344582d23282abf95fc4b8b21a5d93a1f24
ttb0704 <ttb0704@naver.com>
2021-01-25 17:36:47 +0900

    todoList 일부코드수정

--- todo.js
+++ todo.js
@@ -8,14 +8,15 @@ const todos =[];
 
 function loadTodos(){
     const loadTodos = localStorage.getItem(todos_LS);
-        
-    if( loadTodos !== null){
+    console.log( loadTodos );
+    console.log( JSON.parse(loadTodos) );
+    if( loadTodos != null ){
         const parseTodos = JSON.parse(loadTodos);
         parseTodos.forEach(function(todo){
-            console.log(todo);
+            paintToDo(todo.text);
         });
-       
-     }
+    }
+    
 }
 
 function submitHandler(event){
@@ -34,13 +35,14 @@ function paintToDo(text){
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id=newID;
     toDoList.appendChild(li);
     const toDosObj={
         id:newID,
         text:text
     }        
-    todos.push(JSON.stringify(toDosObj));
-    localStorage.setItem(todos_LS,todos);
+    todos.push(toDosObj);
+    localStorage.setItem(todos_LS,JSON.stringify(todos));
     
 }
 
